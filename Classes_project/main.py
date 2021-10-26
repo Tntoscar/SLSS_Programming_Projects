9c42086522d65c4091a785aaa2cce06ce4a7cdef
tntoscar <oscaryu33@gmail.com>
2021-10-26 09:50:05 -0700

    created new code

--- Classes_project/main.py
+++ Classes_project/main.py
@@ -1,17 +1,32 @@
 class vehicle:
-    """ A Class to create a vehicle
-    Attributes/properties:
-        name: str
-        max_speed: int
-        capacity: int
-    """
-    def __init__(self):
-        """creates a new vehicle with it's own"""
-        self.door = 4
-        self.trunk = 1
+    """ A class to represent a car
 
-    def __init__(self, max_speed: int):
-        """creates a max speed of the vehicle"""
-        self.max_speed = 100
+     It's name is Vehicle and it has the
+     following attributes/methods:
+
+    name: str
+        what the name of the car is
+    max_speed: int
+          what the maximum speed the vehicle can go
+    capacity: int
+         how many people vehicle can hold
+    Returns:
+        None
+    """
+    def __init__(self, name: str, capacity: int, max_speed: int):
+        """Creates a new vehicle with new default values"""
+        self.name = vehicle
+        self.max_speed = max_speed
+        self.capacity = capacity
+
+
+    def vroom(self) -> None:
+        """prints vroom the number of times of max speed"""
+        for i in range(self.max_speed):
+            print("VROOMMMM!!!")
+Toyota = vehicle("Toyota", 5, 10)
+print(Toyota.max_speed)
+
+
 
 
