857f77fad203f82c8c7b9f9ef79190fabcb18228
tntoscar <oscaryu33@gmail.com>
2021-10-26 08:51:10 -0700

    created new code

---
+++ Classes_project/main.py
@@ -0,0 +1,17 @@
+class vehicle:
+    """ A Class to create a vehicle
+    Attributes/properties:
+        name: str
+        max_speed: int
+        capacity: int
+    """
+    def __init__(self):
+        """creates a new vehicle with it's own"""
+        self.door = 4
+        self.trunk = 1
+
+    def __init__(self, max_speed: int):
+        """creates a max speed of the vehicle"""
+        self.max_speed = 100
+
+
