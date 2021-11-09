59cb36dce8d17a5f748bcb42ac14a2fd8711565f
tntoscar <oscaryu33@gmail.com>
2021-11-09 08:47:22 -0800

    Added pygame_drawing and completed some boilerplate

---
+++ Halloweenproject/pygame_drawing.py
@@ -0,0 +1,26 @@
+# Pygame_Drawing
+# Author: Oscar
+# 9 November 2021
+
+# Get introduced to Pygame
+
+import pygame
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+def main() -> None:
+    """Driver of the python script"""
+
+
+if __name__ == "__main__":
+    main()
