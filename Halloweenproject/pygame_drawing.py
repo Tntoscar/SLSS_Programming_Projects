3e50edc57bcad4665ef758309ff32a51f7876daa
tntoscar <oscaryu33@gmail.com>
2021-11-09 09:04:54 -0800

    completed boilerplate in pygame-drawing.py

--- Halloweenproject/pygame_drawing.py
+++ Halloweenproject/pygame_drawing.py
@@ -17,9 +17,32 @@ BLUE = (0, 0, 255)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE = "Pygame Drawing"
+
 
 def main() -> None:
     """Driver of the python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(WINDOW_TITLE)
+    # Create the local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+
+    # ------------ Event LISTENER
+    while not done:
+        # make space for the event listener
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        # ------------- CHANGE ENVIRONMENT
+        # ------------- Draw the environment
+        screen.fill(WHITE)      # fill with background color
+        # Update the screen
+        pygame.display.flip()
+
+        # ------------- clock tick
+        clock.tick(75)
 
 
 if __name__ == "__main__":
