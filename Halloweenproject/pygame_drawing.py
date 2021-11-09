445f1fc681a1eea1072ba5fbc5cc9324614ceef8
tntoscar <oscaryu33@gmail.com>
2021-11-09 09:49:36 -0800

    added more drawings

--- Halloweenproject/pygame_drawing.py
+++ Halloweenproject/pygame_drawing.py
@@ -13,6 +13,8 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+AQUA = (66, 239, 245)
+LIGHT_ORANGE = (245, 176, 66)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -38,6 +40,24 @@ def main() -> None:
         # ------------- CHANGE ENVIRONMENT
         # ------------- Draw the environment
         screen.fill(WHITE)      # fill with background color
+        pygame.draw.rect(screen, BLACK, [90, 90, 100, 100])
+        pygame.draw.rect(screen, RED, [100, 100, 75, 75])
+        pygame.draw.rect(screen, BLUE, [110, 110, 50, 50])
+        pygame.draw.circle(screen, AQUA, (600, 255), 120)
+        pygame.draw.circle(screen, BLUE, (600, 255), 100)
+        pygame.draw.circle(screen, GREEN, (600, 255), 80)
+        pygame.draw.circle(screen, RED, (600, 255), 60)
+        pygame.draw.circle(screen, WHITE, (600, 255), 40)
+        pygame.draw.circle(screen, BLACK, (600, 255), 20)
+        pygame.draw.circle(screen, LIGHT_ORANGE, (600, 255), 10)
+
+        pygame.draw.circle(screen, LIGHT_ORANGE, (300, 300), 100)
+        pygame.draw.rect(screen, GREEN, [280, 400, 30, 150])
+        pygame.draw.circle(screen, BLACK, (240, 250), 40)
+        pygame.draw.circle(screen, BLACK, (350, 250), 40)
+        pygame.draw.circle(screen, WHITE, (240, 250), 20)
+        pygame.draw.circle(screen, WHITE, (350, 250), 20)
+        pygame. draw.circle(screen, RED, (350, 250), 10)
         # Update the screen
         pygame.display.flip()
 
