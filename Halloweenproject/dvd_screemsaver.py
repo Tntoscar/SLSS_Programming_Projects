7cbc47933da355dba74bddcf611abd13629fdf1d
tntoscar <oscaryu33@gmail.com>
2021-11-15 09:44:08 -0800

    added x and y velocity

--- Halloweenproject/dvd_screemsaver.py
+++ Halloweenproject/dvd_screemsaver.py
@@ -19,6 +19,7 @@ SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 WINDOW_TITLE = "DVD Screen Saver"
 
+
 class Dvdimage:
     """Represents a dvdimage on screen
 
@@ -27,16 +28,27 @@ class Dvdimage:
         width: width of our rectangle in px
         height:height of our rectangle
         colour: 3-tuple of (r, g, b)
+        x-vel: x velocity in px/sec
+        y-vel: y velocity in px/sec
     """
     def __init__(self):
         self.x, self.y = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         self.width = 150
         self.height = 90
         self.colour = BLUE
+        self.x_vel = 5
+        self.y_vel = 3
 
     def rect(self) -> pygame.rect:
         """Returns a pygame.rect that represents the dvd_image"""
         return [self.x, self.y, self.width, self.height]
+
+    def update(self) -> None:
+        """Updates the Dvdimage with every tick"""
+        # update the x-coordinate
+        self.x += self.x_vel
+        # update the y- coordinate
+        self.y += self.y_vel
 
 
 def main() -> None:
@@ -58,10 +70,10 @@ def main() -> None:
                 done = True
 
         # ----------- CHANGE ENVIRONMENT
-
+        dvd_image.update()
+        print(f"x: {dvd_image.x}, y: {dvd_image.y}")
         # ----------- DRAW THE ENVIRONMENT
         screen.fill(BGCOLOUR)      # fill with bgcolor
-
 
         pygame.draw.rect(screen, dvd_image.colour, dvd_image.rect())
         # Update the screen
