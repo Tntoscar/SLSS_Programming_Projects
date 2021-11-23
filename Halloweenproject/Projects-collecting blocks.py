0fbaccf4c8f4fc0e61ad57f1453eed4809a54908
tntoscar <oscaryu33@gmail.com>
2021-11-23 12:49:39 -0800

    updated player and added points system

--- Halloweenproject/Projects-collecting blocks.py
+++ Halloweenproject/Projects-collecting blocks.py
@@ -2,7 +2,7 @@
 # Author: oscar
 # 2021 Version
 
-
+import random
 import pygame
 
 pygame.init()
@@ -15,12 +15,32 @@ BLUE = (0,   0, 255)
 BGCOLOUR = (100, 100, 255)
 RUST = (148, 27, 12)
 BLIZZARD_BLUE = (154, 229, 230)
-
+PALE_GOLD = (230, 190, 138)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 WINDOW_TITLE = "Collecting Blocks"
+
+
+class Player(pygame.sprite.Sprite):
+    """Describes a player object
+    A subclass of pygame.sprite.Sprite
+    Attributes:
+        image: Surface that is the visual
+            representation of our Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self) -> None:
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.image.load("./images/charmender.png")
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
 
 
 class Block(pygame.sprite.Sprite):
@@ -61,16 +81,34 @@ def main() -> None:
     # Create some local variables that describe the environment
     done = False
     clock = pygame.time.Clock()
-
-    # Create a group of sprites to store ALL SPRITES
-    all_sprites = pygame.sprite.Group()
-
-    # Create the player block
-    player = Block(BLIZZARD_BLUE, 20, 15)
-    # add the player to all_sprites group
-    all_sprites.add(player)
+    num_blocks = 100
+    score = 0
 
     pygame.mouse.set_visible(False)
+
+    # Create groups to hold sprites
+    all_sprites = pygame.sprite.Group()
+    block_sprites = pygame.sprite.Group()
+
+    # Create all the block sprites and add to block_sprites
+    for i in range(num_blocks):
+        # Create a block ( set its parameters)
+        block = Block(WHITE, 20, 15)
+
+        # set a random location for hte block inside the screen
+        block.rect.x = random.randrange(SCREEN_WIDTH - block.rect.width)
+        block.rect.y = random.randrange(SCREEN_HEIGHT - block.rect.height)
+
+        # Add the block to the block_sprite Group
+        # Add the block to the all_sprites Group
+        block_sprites.add(block)
+        all_sprites.add(block)
+
+    # Create the player block
+    player = Player()
+        # Block(PALE_GOLD, 20, 15)
+    # add the player to all_sprites group
+    all_sprites.add(player)
 
     # ----------- MAIN LOOP
     while not done:
@@ -81,11 +119,19 @@ def main() -> None:
 
         # ----------- CHANGE ENVIRONMENT
         # Process player movement based on mouse position
+
         mouse_pos = pygame.mouse.get_pos()
         player.rect.x, player.rect.y = mouse_pos
 
+        # Check all collisions between player and the blocks
+        blocks_collided = pygame.sprite.spritecollide(player, block_sprites, True)
+
+        for block in blocks_collided:
+            score += 1
+            print(f"Score: {score}")
+
         # ----------- DRAW THE ENVIRONMENT
-        screen.fill(RUST)      # fill with bgcolor
+        screen.fill(BLIZZARD_BLUE)      # fill with bgcolor
 
         # Draw all sprites
         all_sprites.draw(screen)
