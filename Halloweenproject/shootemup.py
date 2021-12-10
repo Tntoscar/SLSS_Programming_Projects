65f8afb38227e2161993a3dca65a85644d20fa72
tntoscar <oscaryu33@gmail.com>
2021-12-10 09:44:39 -0800

    created shootemup.py

---
+++ Halloweenproject/shootemup.py
@@ -0,0 +1,280 @@
+import random
+import time
+import pygame
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0,   0,   0)
+RED = (255,   0,   0)
+GREEN = (0, 255,   0)
+BLUE = (0,   0, 255)
+BGCOLOUR = (100, 100, 255)
+RUST = (148, 27, 12)
+BLIZZARD_BLUE = (154, 229, 230)
+PALE_GOLD = (230, 190, 138)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE = "Collecting Blocks"
+
+
+class Player(pygame.sprite.Sprite):
+    """Describes a player object
+    A subclass of pygame.sprite.Sprite
+    Attributes:
+        image: Surface that is the visual
+            representation of our Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+        hp: describes how much health our
+            player has
+
+    """
+    def __init__(self) -> None:
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.image.load("./images/charmender.png")
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
+
+        # Initial health points
+        self.hp = 250
+
+    def hp_remaining(self) -> float:
+        """Return the percent of health remaining"""
+        return self.hp / 250
+
+
+class Enemy(pygame.sprite.Sprite):
+    """The enemy sprites
+
+    Attributes:
+        image: Surface is the visual representation
+        rect: Rect (x, y, width, height)
+        x_vel = x velocity
+        y_vel = y velocity
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("./images/spaceinvaders.png")
+        # Resize the image (scale)
+        self.image = pygame.transform.scale(self.image, (56, 40))
+        # self.image = pygame.transform.scale(self.image, (91, 109))
+
+        self.rect = self.image.get_rect()
+        # define the initial location
+        self.rect.x, self.rect.y = (
+            random.randrange(SCREEN_WIDTH),
+            random.randrange(SCREEN_HEIGHT)
+        )
+
+        # Define the inital velocity
+        self.x_vel = random.choice([-2, -1, 1, 2])
+        self.y_vel = random.choice([-2, -1, 1, 2])
+
+    def update(self):
+        """calculate movement"""
+        self.rect.x += self.x_vel
+        self.rect.y += self.y_vel
+
+        # constrain the movement
+        # X-
+        if self.rect.left < 0:
+            self.rect.x = 0
+            self.x_vel = -self.x_vel  # bounces
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+            self.x_vel = -self.x_vel
+        # Y-
+        if self.rect.y < 0:
+            self.rect.y = 0
+            self.y_vel = - self.y_vel
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+            self.y_vel = -self.y_vel
+
+class Bullet(pygame.sprite.Sprite):
+    """Bullet
+
+    Attribute:
+        image: visual representation
+        rect: mathemetical representation (hit box)
+        vel_y: y velocity in px/sec
+    """
+    def __init__(self, coords: tuple):
+        """
+
+        Arguments:
+             coords: tuple of (x, y) to represent initial location
+        """
+        super().__init__()
+
+        self.image = pygame.Surface((5, 10))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+
+        # set te middle of the bullet to be at coords
+        self.rect.center = coords
+
+        self.vel_y = 3
+
+    def update(self):
+        self.rect.y -= self.vel_y
+
+
+def main() -> None:
+    """Driver of the Python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(WINDOW_TITLE)
+
+    # Create some local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+    num_enemies = 15
+    score = 0
+    time_start = time.time()
+    time_invincible = 5        # Seconds
+    game_state = "running"
+    endgame_cooldown = 5        # seconds
+    time_ended = 0.0
+
+    endgame_messages = {
+        "win": "Congratulations, you won!",
+        "Lose": "Sorry, you are just bad. Play again!",
+    }
+
+    font = pygame.font.SysFont("Arial", 25)
+
+    pygame.mouse.set_visible(False)
+
+    # Create groups to hold sprites
+    all_sprites = pygame.sprite.Group()
+    enemy_sprites = pygame.sprite.Group()
+    bullet_sprites = pygame.sprite.Group()
+
+    # Create enemy sprites
+    for i in range(num_enemies):
+        # Create an enemy
+        enemy = Enemy()
+
+        # add it to the sprites list( enemy_sprites and all_sprites)
+        enemy_sprites.add(enemy)
+        all_sprites.add(enemy)
+
+    # Create the player block
+    player = Player()
+    # Block(PALE_GOLD, 20, 15)
+    # add the player to all_sprites group
+    all_sprites.add(player)
+
+    # ----------- MAIN LOOP
+    while not done:
+        # ----------- EVENT LISTENER
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                # we can't create a bullet when:
+                # initial cool down
+                # when there is three on the screen
+                if len(bullet_sprites) < 3 and time.time() - time_start > time_invincible:
+                    bullet = Bullet(player.rect.midtop)
+
+                    bullet_sprites.add(bullet)
+                    all_sprites.add(bullet)
+
+        # End-game listener
+            # SET THE TIME THAT THE GAME WAS WON
+            if time_ended == 0:
+                time_ended = time.time()
+            # Set parameters to keep the screen alive
+            # wait 4 seconds to kill the screen
+            if time.time() - time_ended >= endgame_cooldown:
+                done = True
+
+        if player.hp_remaining() <= 0:
+            # Indicate to draw a message
+            game_state = "lost"
+
+            # SET THE TIME THAT THE GAME WAS WON
+            if time_ended == 0:
+                time_ended = time.time()
+            # Set parameters to keep the screen alive
+            # wait 4 seconds to kill the screen
+            if time.time() - time_ended >= endgame_cooldown:
+                done = True
+
+
+
+
+         # LOSE CONDITION - PLayer's hp goes below 0
+        if player.hp_remaining() <= 0:
+            done = True
+
+        # ----------- CHANGE ENVIRONMENT
+        # Process player movement based on mouse position
+
+        mouse_pos = pygame.mouse.get_pos()
+        player.rect.x = mouse_pos[0] - player.rect.width / 2
+        player.rect.y = mouse_pos[1] - player.rect.height / 2
+
+        # Update the location of all sprites
+        all_sprites.update()
+
+        # Check all collisions between player and the blocks
+        enemies_collided = pygame.sprite.spritecollide(player, enemy_sprites, False)
+
+        # set a time
+        if time.time() - time_start > time_invincible and game_state != "won":
+            for enemy in enemies_collided:
+                player.hp -= 1
+
+        # ----------- DRAW THE ENVIRONMENT
+        screen.fill(BLIZZARD_BLUE)      # fill with bgcolor
+
+        # Draw all sprites
+        all_sprites.draw(screen)
+
+        # Draw the score on the screen
+        screen.blit(
+            font.render(f"Score: {score}", True, BLACK),
+            (5, 5)
+
+        )
+        # Draw a health bar
+        # Draw the background rectangle
+        pygame.draw.rect(screen, GREEN, [580, 5, 215, 20])
+        # Draw the foreground rectangle which is the remaining health
+        life_remaining = 215 - int(215 * player.hp_remaining())
+        pygame.draw.rect(screen, BLUE, [580, 5, life_remaining, 20])
+
+        # If we've won, draw the text on the screen
+        if game_state == "won":
+            screen.blit(
+                font.render(endgame_messages["win"], True, BLACK),
+                (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            )
+
+        # If we've lose, draw the text on the screen
+        if game_state == "lost":
+            screen.blit(
+                font.render(endgame_messages["Lose"], True, BLACK),
+                (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            )
+
+        # Update the screen
+        pygame.display.flip()
+
+        # ----------- CLOCK TICK
+        clock.tick(75)
+
+
+if __name__ == "__main__":
+    main()
