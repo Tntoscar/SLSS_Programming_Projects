19e954daec18b86c88dfb6ab1edd20982834c3fe
tntoscar <oscaryu33@gmail.com>
2022-01-11 12:45:03 -0800

    implemented turtle drawing

--- Halloweenproject/turtle_drawing,py.py
+++ Halloweenproject/turtle_drawing,py.py
@@ -8,6 +8,7 @@ def main():
     screen = turtle.Screen()
     screen.colormode(255)
 
+    sasuke.color((140, 50, 50))
 
     # Ask the turtle to move around the canvas
     for i in range(1000000):
@@ -17,8 +18,16 @@ def main():
         #sasuke.left(50)
         sasuke.right(100 + i)
         sasuke.left(100)
-        sasuke.backward(100)
-        sasuke.forward(100)
+        #sasuke.backward(100)
+        #sasuke.forward(100)
+        #for i in range(1000):
+            #sasuke.forward(50 + i)
+            #asuke.right(93)
+            #sasuke.color((
+               # (150 + i) % 255,
+                #(50 + i) % 255,
+                #(50 + i) % 255,
+           # ))
     turtle.exitonclick()
 
 if __name__ == "__main__":
